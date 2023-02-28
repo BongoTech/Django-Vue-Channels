c60717ffaa11832a311800b698f0c171b4b38e81
Cory McKiel <c_mckiel@outlook.com>
2023-02-28 11:33:49 -0600

    Initial commit of completed integration of Vue and Django.

---
+++ djangovue/displayVue/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DisplayvueConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "displayVue"
---
+++ djangovue/frontend/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
+const { VueLoaderPlugin } = require('vue-loader')
+
+
+module.exports = {
+  context: __dirname,
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  entry: './src/main.js',
+  output: {
+    path: path.resolve('./assets/webpack_bundles'),
+    publicPath: '/static/webpack_bundles/',
+    filename: "[name]-[hash].js"
+  },
+  plugins: [
+    new BundleTracker({filename: './webpack-stats.json'}),
+    new VueLoaderPlugin()
+  ],
+}
