2c061318f8da69b692167199f4d55803571fd181
Cory McKiel <c_mckiel@outlook.com>
2023-02-28 13:40:19 -0600

    Added Channels/Daphne.

--- djangovue/djangovue/settings.py
+++ djangovue/djangovue/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -129,3 +130,5 @@ WEBPACK_LOADER = {
     'IGNORE': [r'.+\.hot-update.js', r'.+\.map'],
   }
 }
+
+ASGI_APPLICATION = "djangovue.asgi.application"
