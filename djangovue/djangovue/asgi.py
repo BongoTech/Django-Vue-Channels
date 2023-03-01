7cdbbeefd440e50ddb5dea4c99695a8b1d92d030
Cory McKiel <c_mckiel@outlook.com>
2023-02-28 19:59:17 -0600

    Client communicates with server over web socket.

--- djangovue/djangovue/asgi.py
+++ djangovue/djangovue/asgi.py
@@ -9,13 +9,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from displayVue.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangovue.settings")
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        )
     }
 )
