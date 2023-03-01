4e833ddaeb05fb068e22cc459fcec7537758a96b
Cory McKiel <c_mckiel@outlook.com>
2023-02-28 20:59:45 -0600

    Chat app's basic functionality works.

--- djangovue/djangovue/settings.py
+++ djangovue/djangovue/settings.py
@@ -132,3 +132,11 @@ WEBPACK_LOADER = {
 }
 
 ASGI_APPLICATION = "djangovue.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        }
+    }
+}
